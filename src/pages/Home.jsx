8de887af181d1697c89dcc5408a3c946520f979a
Ashim Jung Saud <98077757+ashimjungsaud@users.noreply.github.com>
2024-07-04 17:04:03 +0545

    feat: added new  stats section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 // import { Banner } from '../components/Banner'
 import { Navbar } from '../components/Navbar'
-import Statistics from '../components/Statistics'
+// import Statistics from '../components/Statistics'
 import Service from '../components/Service'
 import Footer from '../components/Footer'
 import AboutUs from '../components/AboutUs'
 import Hero from '../components/Hero'
+import Stats from '../components/Stats'
 
 
 export const Home = () => {
@@ -16,7 +17,8 @@ export const Home = () => {
             {/* <Banner /> */}
             <AboutUs />
             <Service />
-            <Statistics />
+            {/* <Statistics /> */}
+            <Stats />
             <Footer />
         </div>
     )
